c30cb24089f38e16626ae141ea5030ef223f8115
Daniel Celedon <celedondaniel21@gmail.com>
2026-03-26 23:59:40 -0300

    Daniel Celedon 114422
    Subiendo ejercicio resuelto

---
+++ NumerosAleatorios.java
@@ -0,0 +1,24 @@
+//crear un proyecto en java y resolver el siguiente ejercicio:
+//generar una serie de 500 números enteros aleatorios entre 10 y 1000
+//calcular el promedio de estos números así como también la suma total de los mismos.
+//Mostrar el resultado de estos valores obtenidos
+//------..........................................--------------------------------....
+//Nombre: Daniel Celedon 
+//Legajo: 114422
+import java.util.Random;
+
+public class NumerosAleatorios {
+  public static void main(String args[]) {
+    Random NumeroAleatorio = new Random();
+    int i=0,Acumulado=0;
+    float promedio=0;
+    for(i=0;i<=500;i++){
+        int a = 10 + NumeroAleatorio.nextInt(1001);
+        System.out.println("El numero aleatorio es: " + a);
+        Acumulado=a+Acumulado;
+    }
+    promedio=(float) Acumulado/500;
+    System.out.println("La Suma total es: " + Acumulado);
+    System.out.println("El promedio es: " + promedio);
+  }
+}
